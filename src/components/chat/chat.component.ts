93b28a3be17d45e769426c4d2e83b44edcb606a3
Julio Sampaio <juliodssampaio@gmail.com>
2018-08-11 20:35:59 -0300

    Add chat textarea

--- src/components/chat/chat.component.ts
+++ src/components/chat/chat.component.ts
@@ -4,6 +4,27 @@ import { Updates } from '../../decorators/updates.decorator';
 import { AvatarComponent } from '../avatar/avatar.component';
 
 export class ChatComponent extends AbstractComponent {
+  connectedCallback() {
+    super.connectedCallback();
+    this.addTextAreaEvents();
+  }
+
+  addTextAreaEvents() {
+    const textarea = this.shadowRoot.querySelector('.textarea');
+    const placeholder = this.shadowRoot.querySelector('.placeholder');
+    textarea.addEventListener('keyup', (e: KeyboardEvent) => {
+      if (textarea.innerHTML.length > 0) {
+        placeholder.classList.add('hidden');
+      } else {
+        placeholder.classList.remove('hidden');
+      }
+      if (e.keyCode === 13) {
+        textarea.innerHTML = '';
+      }
+      console.log(e);
+    });
+  }
+
   getTemplate() {
     return ChatComponentTemplate;
   }
